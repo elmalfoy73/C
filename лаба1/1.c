0acfeccb30325512942b020b91eef8111c1e824c
Elizabeth Malfoy <40549284+elmalfoy73@users.noreply.github.com>
2021-09-10 11:02:29 +0300

    Лабораторная 1

---
+++ лаба1/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char charVar;
+    float floatVar;
+    scanf("%c", &charVar);
+    scanf("%f", &floatVar);
+    printf("%c\n", charVar);
+    printf("%f", floatVar);
+    return 0;
+}
+
