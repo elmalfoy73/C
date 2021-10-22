8e4e6eec0ea0292bebc927519d2b3a5aa320f22f
Elizabeth Malfoy <40549284+elmalfoy73@users.noreply.github.com>
2021-10-22 10:15:33 +0300

    Лабораторная 4

--- лаба4/main.c
+++ лаба4/main.c
@@ -8,4 +8,9 @@ void main()
     scanf("%d", &num);
     int result = MIN <= num && num <= MAX;
     printf(result ? "YES":"NO");
+
+    int num2 = 0;
+    scanf("%d", &num2);
+    printf ("%d", (num2>>19)%2);
+
 }
