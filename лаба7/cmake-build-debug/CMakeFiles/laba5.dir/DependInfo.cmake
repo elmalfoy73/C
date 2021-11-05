499b9c5cd42cf2b07ba944d159d79ba0d5e43236
Elizabeth Malfoy <40549284+elmalfoy73@users.noreply.github.com>
2021-11-05 11:10:02 +0300

    Лабораторная 7

--- лаба7/cmake-build-debug/CMakeFiles/laba5.dir/DependInfo.cmake
+++ лаба7/cmake-build-debug/CMakeFiles/laba5.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Users/Elizabeth/CLionProjects/laba5/main.c" "/Users/Elizabeth/CLionProjects/laba5/cmake-build-debug/CMakeFiles/laba5.dir/main.c.o"
+  "/Users/Elizabeth/CLionProjects/labaС7/main.c" "/Users/Elizabeth/CLionProjects/labaС7/cmake-build-debug/CMakeFiles/laba5.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "AppleClang")
 
--- лаба7/main.c
+++ лаба7/main.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
 
+//an area (BrE\AmE; general) – площадь (количество пространства, которое покрывает какая-либо плоская поверхность или фигура: The area of a triangle
+struct square
+{
+    int x1;
+    int y1;
+    int lenght;
+};
+int Area(int lengh)
+{
+    return lengh*lengh;
+}
+int Perimetr(int lengh)
+{
+    return lengh*4;
+}
+
+struct printer
+{
+    unsigned ready : 1;
+    unsigned notEnoughtToner : 1;
+    unsigned OPCdrumBreak : 1;
+    unsigned noPapper : 1;
+};
+
 int main() {
     enum OS {Symbian, Embedded_Linux, Windows_CE_VxWorks, QNX};
     enum OS myOS;
     myOS = Symbian;
     printf("OS: %d\n\n", myOS);
 
-    struct square
-    {
-        int lenght;
-    };
-    struct square newSquare = {5};
-    int perimetr = newSquare.lenght * 4;
-    int area = newSquare.lenght * newSquare.lenght;
-    printf("Perimetr: %d, \n", perimetr);
+    struct square newSquare = {1,1, 5};
+    int perimetr = Perimetr(newSquare.lenght);
+    int area = Area(newSquare.lenght);
+    printf("Perimetr: %d\n", perimetr);
     printf("Area: %d\n\n", area);
 
-    struct printer
-    {
-        unsigned ready : 1;
-        unsigned notEnoughtToner : 1;
-        unsigned OPCdrumBreak : 1;
-        unsigned noPapper : 1;
-    };
     union number{
         struct printer myPrinter;
         int num;
@@ -32,5 +45,6 @@ int main() {
     printf("Not enought toner: %u\n", reader.myPrinter.notEnoughtToner);
     printf("OPCdrum break: %u\n", reader.myPrinter.OPCdrumBreak);
     printf("No papper: %u\n", reader.myPrinter.noPapper);
+
     return 0;
 }
